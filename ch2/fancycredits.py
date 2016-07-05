93a24c26caef51a335f4b92a962cc2bb031eeeb0
jay <fish@skynet.home>
2016-07-05 21:05:04 +0100

    adding fancycredits.py

---
+++ ch2/fancycredits.py
@@ -0,0 +1,20 @@
+# Fancy Credits
+# Learning to use Escape Sequences with Strings
+# Jay 2016-07-05 20:43:33
+
+print("\t\t\tFancy Credits")
+
+
+print("\t\t\t \\ \\ \\ \\ \\ \\ \\")
+print("\t\t\t\tby")
+print("\t\t\tJay Millington")
+print("\t\t\t \\ \\ \\ \\ \\ \\ \\")
+
+
+print("\nSpecial Thanks goes out to:")
+print("My hair Stylist, Henry \'The Great,\' who never says \"can\'t.\"")
+
+# sound the system bell
+print("\a")
+
+input("\n\nPress the enter key to exit.")
