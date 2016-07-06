7fd346dc6072ff4c761ef1ed659048eff5636bae
jay <fish@skynet.home>
2016-07-06 17:56:54 +0100

    finished personal greeter

---
+++ ch2/personalgreeter.py
@@ -0,0 +1,11 @@
+# Personal Greeter
+# Demonstrates getting user input
+# Jay 2016-07-06 16:21:49
+
+name = input("Hi. What's your name? ")
+
+print(name)
+
+print("Hi,", name)
+
+input("\n\nPress any key to exit...")
