805c7e3591a9f163371b5a817b1f9f245cb22273
jay <fish@skynet.home>
2016-07-07 22:14:28 +0100

    added 2-4 car salesman to finish at work

---
+++ ch2/2-4.py
@@ -0,0 +1,4 @@
+# Car Salesman Program
+# Write a Car Salesman program where the user enters the base price of a car. The program should add on a bunch of extra fees such as tax, license, dealer prep, and destination charge. Make tax and license a percent of the base price. The other fees should be set values. Display the actual price of the car once all the extras are applied.
+# Jay 2016-07-07 21:48:12
+
