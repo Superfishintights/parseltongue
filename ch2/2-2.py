f77de01c24a1faa0a0ecda4920813c750f9ebc67
jay <fish@skynet.home>
2016-07-07 20:09:57 +0100

    added useless trivia, challenge 1 and challenge 2

---
+++ ch2/2-2.py
@@ -0,0 +1,11 @@
+# Challenge 2
+# Write a program that allows a user to enter his or her two favorite foods. The program should then print out the name of a new food by joining the original food names together.
+# Jay 2016-07-07 20:04:56
+
+print("Today we're going to create a new food based off your favourite food choices..")
+
+food1 = input("\nPlease enter your favourite food here.. ")
+food2 = input("\nPlease enter your second favourite food here... ")
+
+print("\nYour new dish to unveal to the world is: ", food1 + food2)
+
