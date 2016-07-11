3f9d7fbcbb799761d138a90452c707dcf05eebc7
jay <fish@skynet.home>
2016-07-11 20:57:08 +0100

    uploaded broken losingbattle.py

---
+++ ch3/threeyearold.py
@@ -0,0 +1,15 @@
+# Three Year-Old Simulator
+# Demonstrates the while loop
+# Jay 2016-07-11 20:15
+
+print("\tWelcome to the 'Three-Year-Old Simulator'\n")
+print("This program simulates a conversation witha  three-year-old child.")
+print("Try to stop the madness.\n")
+
+response = ""
+while response != "Because.":
+        response = input("Why?\n")
+
+print("Oh.  Okay.")
+
+input("\n\nPress the enter key to exit.")
